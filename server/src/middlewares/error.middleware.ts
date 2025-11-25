482c7589421ad641d1fc2b67d523b839001b35e6
ghulammf <muhtadinamri123@gmail.com>
2025-11-25 09:30:47 +0700

    create error middleware

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+class ResponseError extends Error {
+  constructor(public status: number, public message: string) {
+    super(message);
+  }
+}
+
+const errorMiddleware = async (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error instanceof ResponseError) {
+    response.status(error.status).json({
+      message: error.message,
+    });
+  } else {
+    response.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export default errorMiddleware;
