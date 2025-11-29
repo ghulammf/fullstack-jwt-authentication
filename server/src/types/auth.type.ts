876595822f133bd68de69b1a5552e98b60bc633c
ghulammf <muhtadinamri123@gmail.com>
2025-11-29 11:25:45 +0700

    create authentication format type

---
+++ server/src/types/auth.type.ts
@@ -0,0 +1,50 @@
+import { Request } from "express";
+import { Role } from "./user.type";
+
+interface RegisterRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterResponse {
+  username: string;
+  email: string;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+interface LoginResponse {
+  user: {
+    username: string;
+    email: string;
+    role: Role;
+  };
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface JwtPayload {
+  username: string;
+  email: string;
+  role: Role;
+}
+
+interface AuthRequest extends Request {
+  user?: JwtPayload;
+}
+
+export {
+  RegisterRequest,
+  RegisterResponse,
+  LoginRequest,
+  LoginResponse,
+  JwtPayload,
+  AuthRequest,
+};
