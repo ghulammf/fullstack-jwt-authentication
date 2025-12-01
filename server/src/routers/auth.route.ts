d92d9abc8a21203e4413aeb217ab929b34121517
ghulammf <muhtadinamri123@gmail.com>
2025-12-01 23:21:52 +0700

    create auth router

---
+++ server/src/routers/auth.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import AuthController from "../controllers/auth.controller";
+
+const authRouter = Router();
+
+authRouter.post("/api/auth/register", AuthController.register);
+authRouter.post("/api/auth/login", AuthController.login);
+authRouter.post("/api/auth/logout", AuthController.logout);
+authRouter.post("/api/refresh-token", AuthController.refreshToken);
+
+export default authRouter;
